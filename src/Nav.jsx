37de643e13bfbbd064ace052babd4a44c7e8253e
Code_with_Moon <tahaminachy43@gmail.com>
2023-11-09 16:59:35 -0700

    CSS for the nav

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,10 +2,10 @@
 
 export default function Nav() {
     return (
-        <div>
-            <img src="./assets/Unknown.png"/>
-            <h4>ReactFacts</h4>
-            <h3>React Course - Project 1</h3>
+        <div className = "nav">
+            <img src ="./assets/Unknown.png"/>
+            <h1>ReactFacts</h1>
+            <h4>React Course - Project 1</h4>
 
         </div>
         
