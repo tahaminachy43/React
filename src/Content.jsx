0433df72be268109225bbc7f665d8163d41736e3
Code_with_Moon <tahaminachy43@gmail.com>
2023-11-09 17:37:15 -0700

    css changes and react

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,3 +1,19 @@
 export default function Main() {
-    return <h1>Main component</h1>
+    return (
+        <div className ="content">
+
+            <h1 className="contentHeading"> Fun facts about React</h1>
+
+            <ul className="contentlist">
+                <li> Was first released in 2013</li>
+                <li> Was original created by Jordan Walke</li>
+                <li> Has well over 100K stars on GitHub</li>
+                <li> Is maintained by Facebook</li>
+                <li> Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+
+
+        </div>
+    )
 }
+
