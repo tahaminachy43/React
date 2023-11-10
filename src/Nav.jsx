f52da491bae300dea6bb5c385cdc0e5c512afa08
Code_with_Moon <tahaminachy43@gmail.com>
2023-11-09 17:54:44 -0700

    And Done

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,7 +3,7 @@
 export default function Nav() {
     return (
         <div className = "nav">
-            <img src ="./assets/Unknown.png"/>
+            <img src ="./assets/react-icon-small.png"/>
             <h1>ReactFacts</h1>
             <h4>React Course - Project 1</h4>
 
